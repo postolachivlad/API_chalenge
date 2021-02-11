a0e5f7430ebcef3bdcad6c86fd0b0a5cc82a61e7
Vlad Postolachi <postolachi.vlad@gmail.com>
2021-02-11 18:18:24 +0100

    create Destroy action in Message model, create destroy route

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::MessagesController < Api::V1::BaseController
-  before_action :set_message, only: [ :show ]
+  before_action :set_message, only: [:show, :destroy]
 
   def index
     @messages = Message.all
   end
 
   def show
+  end
+
+  def destroy
+    @message.destroy
+    head :no_content
   end
 
   private
