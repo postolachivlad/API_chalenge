deb2552d92e601c468be444879416a6dca6e3392
Vlad Postolachi <postolachi.vlad@gmail.com>
2021-02-12 00:02:37 +0100

    Add Create action in Messages controler, add Create route, change name of column in DB from message to content

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -9,11 +9,14 @@ class Api::V1::MessagesController < Api::V1::BaseController
   end
 
   def update
-    if @message.update(message_params)
-      render :show
-    else
-      render_error
-    end
+    @message.update(message_params) ? (render :show) : render_error
+  end
+
+  def create
+    # Check if Message already exist
+    @message = Message.find_by(content: params[:message][:content])
+    # If exist increment It's counter in DB else create new Message
+    @message ? counter_increment(@message) : create_new_message
   end
 
   def destroy
@@ -28,11 +31,22 @@ class Api::V1::MessagesController < Api::V1::BaseController
   end
 
   def message_params
-    params.require(:message).permit(:message, :counter)
+    params.require(:message).permit(:content, :counter)
   end
 
   def render_error
     render json: { errors: @message.errors.full_messages },
-      status: :unprocessable_entity
+           status: :unprocessable_entity
+  end
+
+  def create_new_message
+    @message = Message.new(message_params)
+    @message.save ? (render :show, status: :created) : render_error
+  end
+
+  def counter_increment(message)
+    message.counter += 1
+    message.save
+    render :show, status: :created
   end
 end
