482a7cdf403d04110f24eeb77be7b9d6dc65d4e8
Vlad Postolachi <postolachi.vlad@gmail.com>
2021-02-12 01:37:01 +0100

    Changed DB to postgresql to be ablo to deploy on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2021_02_11_153745) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", id: :string, limit: 36, force: :cascade do |t|
     t.string "content"
-    t.integer "counter", default: 0
+    t.integer "counter", default: 1
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
